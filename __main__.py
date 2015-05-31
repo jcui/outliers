f74d4920597cd675cb3bb285edda99ac4bb639e7
jcui <jcui@foobar.com>
2015-05-31 11:00:22 -0400

    refactor main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,11 @@
 import argparse
 from flask_app import flask_app
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--debug", action="store_true")
-args = parser.parse_args()
-flask_app.run(args.debug)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    flask_app.run(args.debug)
+
+if __name__ == '__main__':
+    main()
