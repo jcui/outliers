0c69e1476e8f49af1bfd101cad152a08133b0890
jcui <jcui@foobar.com>
2015-05-31 15:27:16 -0400

    fix warnings

--- detection/detection.py
+++ detection/detection.py
@@ -6,8 +6,8 @@ def dict_subset(key_list, input_dict):
     return {key : input_dict[key] for key in key_list if key in input_dict}
 
 def is_outlier(throughput, average, threshold):
-    return average > 0.00000000001 \
-           and (abs(throughput - average) / float(average)) > (threshold / 100.0)
+    return average > 0.00000000001 and \
+           (abs(throughput - average) / float(average)) > (threshold / 100.0)
 
 def get_outliers_throughput(caches_throughput, average, threshold):
     return {uuid : throughput
@@ -19,8 +19,9 @@ def find_outliers(caches_in_cluster, all_caches_throughput, threshold):
                                     all_caches_throughput)
     cluster_average = 0
     outliers_throughput = {}
-    if (len(caches_throughput) > 0):
-        cluster_average = sum(caches_throughput.values()) / len(caches_throughput)
+    if len(caches_throughput) > 0:
+        cluster_average = \
+            sum(caches_throughput.values()) / len(caches_throughput)
         outliers_throughput = get_outliers_throughput(caches_throughput,
                                                       cluster_average,
                                                       threshold)
